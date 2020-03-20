a42838526c446945ee6e9a46146e014ecaf46f3f
markoleht <54833627+markoleht@users.noreply.github.com>
2020-03-20 16:41:54 +0200

    added visual validation via applitools

--- steps/home/then.js
+++ steps/home/then.js
@@ -5,14 +5,14 @@ const Then = cucumber.Then;
 Then("the browser will redirect the user to the facebook page", () => {
     setTimeout(() => {
         browser.getUrl("https://www.facebook.com/markoleht/");
-    },2000);
+    },3000);
 });
 
 
 Then("the browser will redirect the user to the instagram page", () => {
     setTimeout(() => {
         browser.getUrl("https://www.instagram.com/markoleht/");
-    },2000);
+    },3000);
 });
 
 
@@ -21,8 +21,18 @@ Then("the browser will redirect the user to the instagram page", () => {
 Then("the browser will redirect the user to the imdb page", () => {
     setTimeout(() => {
         browser.getUrl("https://www.imdb.com/name/nm4715250/");
-    },2000);
+    },3000);
 });
 
 
-
+Then("the browser will redirect the user to another page" , () => {
+try {
+    browser.call(() => eyes.open(browser));
+    browser.call(() => eyes.check("page", Target.window().fully()));
+    browser.call(() => eyes.close(false));
+} catch (e) {
+console.log(e);
+} finally {
+    browser.call(() => eyes.abortIfNotClosed());
+}
+});
